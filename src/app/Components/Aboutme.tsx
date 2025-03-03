2b9eeba0160866e40bfcf7e24d8ef8a7b341bf4f
Ravi Junnu <ssvkusumamungi@gmail.com>
2025-03-03 12:20:27 +0530

    changes recevie

--- src/app/Components/Aboutme.tsx
+++ src/app/Components/Aboutme.tsx
@@ -1,38 +1,34 @@
 export const Aboutme = () => {
     return (
         <div className="about-me-container" id="home">
-            <div className="title-name">
-                <h1 className="title">ABOUT ME</h1>
+        <div className="title-name">
+            <h1 className="title">ABOUT ME</h1>
+        </div>
+    
+        <div className="content-section">
+            <div className="home">
+                <p className="description">
+                    Hello! I&apos;m <span className="name-highlight">Kusuma Mungi</span>, a passionate and detail-oriented 
+                    web developer with a robust foundation in modern web technologies, including 
+                    React.js, Next.js, JavaScript, HTML, and CSS. I excel at crafting responsive, 
+                    dynamic, and user-friendly web applications, with expertise in state management 
+                    using Redux Toolkit and seamless REST API integration.<br />
+    
+                    I hold a Bachelor of Technology in Electrical &amp; Electronics Engineering from Pragati Engineering College, 
+                    where I developed a strong technical background and successfully 
+                    completed my project titled &quot;Implementation of solar PV system with UAPF.&quot; 
+                </p>
             </div>
-
-            <div className="content-section">
-               
-                <div className="home">
-                    <p className="description">
-                        Hello! I'm <span className="name-highlight">Kusuma Mungi</span>, a passionate and detail-oriented 
-                        web developer with a robust foundation in modern web technologies, including 
-                        React.js, Next.js, JavaScript, HTML, and CSS. I excel at crafting responsive, 
-                        dynamic, and user-friendly web applications, with expertise in state management 
-                        using Redux Toolkit and seamless REST API integration.<br></br>
-
-                        I hold a Bachelor of Technology in Electrical & Electronics Engineering from Pragati Engineering College 
-                        , where I developed a strong technical background and successfully 
-                        completed my project titled "Implementation of solar PV system with UAPF." 
-            
-                       
-                    </p>
-                </div>
-
-              
-                <div className="profile-image-container1">
-                    <img 
-                        src="c../../WhatsApp Image 2025-02-27 at 8.55.04 PM.jpeg" 
-                        alt="Kusuma Mungi " 
-                        className="profile-image1" 
-                    />
-                </div>
+    
+            <div className="profile-image-container1">
+                <img 
+                    src="../../WhatsApp Image 2025-02-27 at 8.55.04 PM.jpeg" 
+                    alt="Kusuma Mungi" 
+                    className="profile-image1" 
+                />
             </div>
         </div>
-    );
+    </div>
+        );
 };
 
